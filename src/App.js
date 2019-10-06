c2d95d8f7a59db5f41cdadaf67e12688ee522c1e
Hejmej <alexhejmej@gmail.com>
2019-10-06 01:43:30 -0400

    merging code into first demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import Home from './components/home';
+import Discover from './components/discover';
+import Analytics from './components/analytics';
+import Search from './components/search';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/discover" component={Home}/>
+        <Header/>
+      </div>
+    </BrowserRouter>
   );
 }
 
