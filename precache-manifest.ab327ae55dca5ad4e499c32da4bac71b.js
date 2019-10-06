26660e8b16e0a973d1afeac38564c8edee2e9c99
Owen Matsuda <owen.matsuda@gmail.com>
2019-10-06 04:27:44 -0400

    Updates

--- precache-manifest.ab327ae55dca5ad4e499c32da4bac71b.js
+++ precache-manifest.ab327ae55dca5ad4e499c32da4bac71b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5aa10dc57bbce166e81035cbdfd0067d",
+    "revision": "0d37a483e3a4b6787b32ff4ef657d391",
     "url": "/index.html"
   },
   {
-    "revision": "a54bc7890ae8f1971774",
+    "revision": "27bf36717871cb8c7b87",
     "url": "/static/css/main.f05016c4.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.37a9d189.chunk.js"
   },
   {
-    "revision": "a54bc7890ae8f1971774",
-    "url": "/static/js/main.6b474372.chunk.js"
+    "revision": "27bf36717871cb8c7b87",
+    "url": "/static/js/main.415b2833.chunk.js"
   },
   {
     "revision": "933fe2844116aacf933f",
